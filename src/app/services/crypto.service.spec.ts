9ad5b29f4cee58d04034e704d8c2a36f497eedc5
Felipe Sampaio <fmarques899@gmail.com>
2017-08-16 17:13:48 -0300

    Crypto Service Tests

--- src/app/services/crypto.service.spec.ts
+++ src/app/services/crypto.service.spec.ts
@@ -12,4 +12,17 @@ describe('CryptoService', () => {
   it('should be created', inject([CryptoService], (service: CryptoService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should encrypt a message', inject([CryptoService], (service: CryptoService) => {
+    const message = 'Test';
+    const key = 'test key';
+
+    expect(service.encrypt(message, key)).not.toBe(message);
+  }));
+
+  it('should decrypt a message', inject([CryptoService], (service: CryptoService) => {
+    const cryptedMsg = "Cq4G8DlLKB90qvhuZBMjcg==";
+    const key = 'test key';
+    expect(service.decrypt(cryptedMsg, key)).not.toBe(cryptedMsg);
+  }))
 });
