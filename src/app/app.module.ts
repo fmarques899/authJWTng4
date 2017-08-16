71fc4ba5c3b14880c7cf068c7907fafa6961f1a2
João Henrique P. de Almeida <joaohenrique@Mac-mini-de-Yan.local>
2017-08-16 16:33:07 -0300

    Add sign_up component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule }    from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign_up/sign_up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sign_up/sign_up.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+
+import { FormGroup, FormBuilder,Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign_up.component.html',
+  styleUrls: ['./sign_up.component.css']
+})
+export class SignUpComponent implements OnInit {
+
+  signUpForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    
+  ) { 
+    this.signUpForm = fb.group({
+      'username':[null,Validators.required],
+      'cpf':[null,Validators.required]
+,     'password':[null,Validators.compose([Validators.required])],
+      'password_confirmation': [null,Validators.required],
+      'email': [null,Validators.compose([Validators.email,Validators.required])],
+      'email_confirmation': [null,Validators.compose([Validators.email,Validators.required])]
+    },
+    {
+      validatorPassword: this.checkIfMatchingPasswords('password', 'password_confirmation'),
+      validatorEmail : this.checkIfMatchingEmails('email', 'email_confirmation')  
+  },)
+  }
+
+  checkIfMatchingPasswords(passwordKey: string, passwordConfirmationKey: string) {
+          return (group: FormGroup) => {
+            let passwordInput = group.controls[passwordKey],
+                passwordConfirmationInput = group.controls[passwordConfirmationKey];
+            if (passwordInput.value !== passwordConfirmationInput.value) {
+              return passwordConfirmationInput.setErrors({notEquivalent: true})
+            }
+            else {
+                return passwordConfirmationInput.setErrors(null);
+            }
+          }
+        }
+
+  checkIfMatchingEmails(emailKey: string, emailConfirmationKey: string) {
+          return (group: FormGroup) => {
+            let emailInput = group.controls[emailKey],
+                emailConfirmationInput = group.controls[emailConfirmationKey];
+            if (emailInput.value !== emailConfirmationInput.value) {
+              return emailConfirmationInput.setErrors({notEquivalent: true})
+            }
+            else {
+                return emailConfirmationInput.setErrors(null);
+            }
+          }
+        }      
+
+
+
+
+  ngOnInit() {
+  }
+
+}
