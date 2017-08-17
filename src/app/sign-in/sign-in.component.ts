4390bb5a1b13340ce0fd2a6b4713e44bfa9dfa23
joaohenriquepda <joaohenrique.p.almeida@gmail.com>
2017-08-17 11:34:14 -0300

    Add layout and simple validations in SignInComponent

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators }  from '@angular/forms'
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  signInForm : FormGroup;  
+  
+  constructor(
+    private fb : FormBuilder,
+  ) {
+    
+    this.signInForm = fb.group({
+      'username':[null,Validators.compose([Validators.required])],
+      'cpf': [null,Validators.compose([Validators.required])],  
+      'email': [null,Validators.compose([Validators.required, Validators.email])],
+      'password':[null,Validators.compose([ Validators.required ])],
+    },
+  
+  )}
 
   ngOnInit() {
+  }
+
+
+  signIn(){
+    console.log("Login");
+    
+  }
+
+  resetFields(){
+    this.signInForm.reset();
   }
 
 }
