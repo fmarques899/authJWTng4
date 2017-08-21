9ebf37fbe0be6d26f5966ee67e1f3060a60c59cf
Felipe Sampaio <fmarques899@gmail.com>
2017-08-21 17:21:56 -0300

    UserService sigIn and its component

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response, RequestOptions } from '@angular/http'
 import 'rxjs/add/operator/map'
-import { CryptoService } from '../services/crypto.service' 
+import { CryptoService } from '../services/crypto.service'
 
- 
+
 @Injectable()
 export class UserService {
 
-  user : any = {};  
+  user : any = {};
 
-  constructor( 
+  constructor(
     private http : Http,
     private cryptoService : CryptoService
   ) { }
@@ -18,31 +18,35 @@ export class UserService {
     let ui : any
     ui = {user}
 
-    return this.http.post('http://localhost:3000/users',ui).map((response : Response)=> 
-    { 
+    return this.http.post('http://localhost:3000/users',ui).map((response : Response)=>
+    {
       response.json();
     }
     );
-  
+
   }
 
   signIn(user : any){
-    return this.http.get('https://jsonplaceholder.typicode.com/users',user ).map((response : Response)=>{
-    let cryptToken = this.cryptoService.encrypt('token',response[`token`])  
-    
-  }); 
+    return this.http.get('http://jsonplaceholder.typicode.com/users',user ).map((response : Response)=>{
+    let cryptToken = this.cryptoService.encrypt(response.toString(), 'key is 123');
+    localStorage.setItem('token', cryptToken);
+
+    return response;
+  }, error => {
+
+  });
 
   }
 
   fetch(){
      return this.http.get('https://jsonplaceholder.typicode.com/users',this.jwt).map((response : Response)=>{
-      //Set information for user in  
-  }); 
+      //Set information for user in
+  });
 
   }
-  
+
   logout(){
-  
+
   }
 
   refresh(){
@@ -57,11 +61,11 @@ export class UserService {
     //create authorization
     let currentUser = JSON.parse(sessionStorage.getItem('token'))
     this.cryptoService.decrypt('token',currentUser);
-   
+
     if(currentUser && currentUser.token){
       let headers = new  Headers({'Authorization':'Bear ' + currentUser.token});
       return new RequestOptions({headers: headers});
-    }  
+    }
 
   }
 
--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators }  from '@angular/forms'
+import { UserService, AlertService } from '../services/index';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -8,28 +10,42 @@ import { FormBuilder, FormGroup, Validators }  from '@angular/forms'
 })
 export class SignInComponent implements OnInit {
 
-  signInForm : FormGroup;  
-  
+  signInForm : FormGroup;
+
   constructor(
     private fb : FormBuilder,
+    private userService: UserService,
+    private alertService: AlertService,
   ) {
-    
+
     this.signInForm = fb.group({
       'username':[null,Validators.compose([Validators.required])],
-      'cpf': [null,Validators.compose([Validators.required])],  
+      'cpf': [null,Validators.compose([Validators.required])],
       'email': [null,Validators.compose([Validators.required, Validators.email])],
       'password':[null,Validators.compose([ Validators.required ])],
     },
-  
+
   )}
 
   ngOnInit() {
   }
 
 
-  signIn(){
+  signIn(user : any){
     console.log("Login");
-    
+    this.userService.signIn(user).subscribe(
+      data=>{
+        console.log(JSON.stringify(data))
+      },
+      error=>{
+
+        console.log(JSON.stringify(error) + "error!!!");
+
+        this.alertService.success(JSON.stringify(error))
+      }
+
+    )
+
   }
 
   resetFields(){
